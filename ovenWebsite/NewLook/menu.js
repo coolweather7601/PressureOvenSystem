5b7bee02b052c17b4042d6daad6aa753da652ec6
Alan Kuo <coolweather7601@gmail.com>
2014-04-09 13:19:09 +0800

    newlook

--- ovenWebsite/NewLook/menu.js
+++ ovenWebsite/NewLook/menu.js
@@ -7,7 +7,6 @@ _page.hideLeftNavigation = true;
 //_page.items["0"]        = new _Item("List","../others/listIndex.aspx")
 
 _page.items["0"] = new _Item("Check-In", "../Others/CheckIn.aspx")
-_page.items["0_1"] = new _Item("Check-In", "../Others/CheckIn.aspx")
 
 _page.items["1"] = new _Item("PTN Maintain", "../Others/PTN_maintain.aspx")
 _page.items["1_1"] = new _Item("PTN Maintain", "../Others/PTN_maintain.aspx")
@@ -15,9 +14,9 @@ _page.items["1_2"] = new _Item("Oven Manage", "../Others/oven_manage.aspx")
 
 //_page.items["2"] = new _Item("Chart_Index", "../Chart_Index/Index.htm")
 
-_page.items["2"] = new _Item("trendline", "../Chart/chart_trendline.aspx")
-_page.items["2_1"] = new _Item("trendline", "../Chart/chart_trendline.aspx")
-_page.items["2_2"] = new _Item("multiaxes", "../Chart/chart_multiaxes.aspx")
+_page.items["2"] = new _Item("Trendline", "../Chart/chart_trendline.aspx")
+_page.items["2_1"] = new _Item("Trendline", "../Chart/chart_trendline.aspx")
+_page.items["2_2"] = new _Item("Multiaxes", "../Chart/chart_multiaxes.aspx")
 /* Define the site menu structure using an associative array */
 _page.sites[0] = new _Site("Semiconductors", "http://twgkhhpsk1ms011 ")
 _page.sites[1] = new _Site("APK","http://twgkhhpsk1ms011/imo/backend/kaohsiung/")
